7367a7ec69a2fb3ee0c973a641ae8c1ae515ed28
simon19833 <151372355+simon19833@users.noreply.github.com>
2026-03-22 21:53:41 -0300

    Add MyToken contract (ERC20 basic)

---
+++ day1-erc20/MyToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MyToken {
+
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply;
+        balanceOf[msg.sender] = initialSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+}
