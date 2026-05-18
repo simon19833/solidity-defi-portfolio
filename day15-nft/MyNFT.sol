6e78250dc2b2a543cd5b3c8b1d5bdd7a2b93a936
simon19833 <151372355+simon19833@users.noreply.github.com>
2026-05-17 22:08:42 -0400

    Add Day 15 NFT contract - deployed on Sepolia

---
+++ day15-nft/MyNFT.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+
+    uint256 public tokenCounter;
+    uint256 public maxSupply;
+    uint256 public mintPrice;
+
+    constructor(
+        uint256 _maxSupply,
+        uint256 _mintPrice
+    ) ERC721("MyNFT", "MNFT") Ownable(msg.sender) {
+        maxSupply = _maxSupply;
+        mintPrice = _mintPrice;
+        tokenCounter = 0;
+    }
+
+    function mint() external payable {
+        require(tokenCounter < maxSupply, "Sold out");
+        require(msg.value >= mintPrice, "Not enough ETH");
+        tokenCounter++;
+        _safeMint(msg.sender, tokenCounter);
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Nothing to withdraw");
+        (bool success, ) = payable(msg.sender).call{value: balance}("");
+        require(success, "Transfer failed");
+    }
+}
